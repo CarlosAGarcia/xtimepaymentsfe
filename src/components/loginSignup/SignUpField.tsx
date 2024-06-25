ac37f15f38b4ab9bed632cf3cd5d35b69de78732
Carlos <thethirdcage@gmail.com>
2024-06-26 03:09:44 +0800

    sign up finalized w token saving to local storage

--- src/components/loginSignup/SignUpField.tsx
+++ src/components/loginSignup/SignUpField.tsx
@@ -1,16 +1,33 @@
-import React, { useContext, useEffect } from 'react';
-import { Button, TextField } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, IconButton, InputAdornment, TextField, Link } from '@mui/material';
 import axios from 'axios';
 import { UserContext } from '../../contexts/users/users';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 let source: any;
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const SignUpField: React.FC = () => {
+    const navigate = useNavigate();
+
+    // CONTEXT START
     const userContext = useContext(UserContext);
     if (!userContext) throw new Error('YourComponent must be used within a MainProvider');
-
-    const { isEmailAvailable, getIsEmailAvailable } = userContext;
+    const { isEmailAvailable, getIsEmailAvailable, signUp, signUpVars } = userContext;
+    const { signUpSuccess, signUpLoading, signUpErr } = signUpVars;
     const { isEmailAvailableLoading, isEmailAvailableErr, isEmailAvailable: isEmailAvailableResult } = isEmailAvailable;
+    // CONTEXT END
+
+
+    const [email, setEmail] = React.useState<string>('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [isValidEmail, setIsValidEmail] = useState(false);
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
+    const [passwordError, setPasswordError] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
     // on mount it sets up the axios cancel token and cancels reqs if unmounts
     useEffect(() => {
@@ -21,25 +38,129 @@ const SignUpField: React.FC = () => {
     }
     , []);
 
-    // stores user email and updates it on change
-    const [email, setEmail] = React.useState<string>('');
-    const onChangeEmail = (e: any) => {
-        setEmail(e.target.value);
-    }
+    // email validity
+    useEffect(() => {
+        setIsValidEmail(emailRegex.test(email));
+    }, [email]);
+
+    // password validity
+    useEffect(() => {
+        if (password.length < 12 || !/[!@#$%^&*]/.test(password)) {
+            setPasswordError('Password must be at least 12 characters and include at least one special character.');
+            setIsPasswordValid(false);
+        } else {
+            setPasswordError('');
+            setIsPasswordValid(true);
+        }
+
+        if (confirmPassword && password !== confirmPassword) {
+            setConfirmPasswordError('Passwords do not match.');
+        } else {
+            setConfirmPasswordError('');
+        }
+    }, [password, confirmPassword]);
+
+    // when signUpLoading changes and signUpSuccess is true, it redirects to login after 3 seconds
+    useEffect(() => {
+        if (signUpLoading) {
+            setTimeout(() => {
+                if (signUpSuccess) {
+                   // redirect to /dashboard
+                   navigate('/dashboard')
+                }
+            }, 3000);
+        }
+    }, [signUpLoading, signUpSuccess, navigate]);
 
     const onContinueClick = () => {
-        if (getIsEmailAvailable !== undefined) {
+        if (isValidEmail) {
             getIsEmailAvailable(email, source);
         }
+    };
+
+    const onSignup = () => {
+        // calls signUp from context
+        signUp({ email, password }, source);
     }
 
-    console.log('isEmailAvailableResult:', isEmailAvailableResult, 'isEmailAvailableLoading:', isEmailAvailableLoading, 'isEmailAvailableErr:', isEmailAvailableErr);
     return (
         <>
-            <TextField label="Email address" variant="outlined" fullWidth sx={{ mb: 2 }} onChange={onChangeEmail}/>
-            <Button variant="contained" fullWidth sx={{ mb: 2 }} onClick={onContinueClick}>
-                Continue
-            </Button>
+                {!isEmailAvailableResult ? (
+                    <>
+                        <TextField
+                            label="Email address"
+                            variant="outlined"
+                            fullWidth
+                            sx={{ mb: 2 }}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            error={!isValidEmail && email.length > 0}
+                            helperText={!isValidEmail && email.length > 0 ? 'Please enter a valid email address.' : ''}
+                        />
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ mb: 2 }}
+                            onClick={onContinueClick}
+                            disabled={!isValidEmail || isEmailAvailableLoading}
+                        >
+                            {isEmailAvailableLoading ? 'LOADING...' : 'Continue'}
+                        </Button>
+                    </>
+                ) : (
+                    <>
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            fullWidth
+                            sx={{ mb: 2 }}
+                            type={showPassword ? 'text' : 'password'}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            error={Boolean(passwordError)}
+                            helperText={passwordError}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={() => setShowPassword(!showPassword)}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <TextField
+                            label="Confirm Password"
+                            variant="outlined"
+                            fullWidth
+                            sx={{ mb: 2 }}
+                            type={showPassword ? 'text' : 'password'}
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            error={Boolean(confirmPasswordError)}
+                            helperText={confirmPasswordError}
+                        />
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ mb: 2 }}
+                            disabled={!isPasswordValid || password !== confirmPassword}
+                            onClick={onSignup}
+                        >
+                            Submit
+                        </Button>
+
+                        { signUpSuccess && (
+                            <>
+                                <p>Sign up successful! Redirecting to login...</p>
+                                <Link href="/dashboard" sx={{ mx: 1 }}>or click here if you're not redirected</Link>
+                            </>
+                        )}
+                    </>
+                )}
         </>
     );
 };
--- src/contexts/users/users.tsx
+++ src/contexts/users/users.tsx
@@ -14,12 +14,13 @@ interface UserContextType {
     // LOGIN - REGISTER purposes
     isEmailAvailable: { isEmailAvailable: boolean, isEmailAvailableLoading: boolean, isEmailAvailableErr: boolean };
     getIsEmailAvailable: (email: string, source?: any) => void;
+
+    // Sign up
+    signUp: (vars: { email: string, password: string }, source?: any) => void;
+    signUpVars: { signUpSuccess: boolean, signUpLoading: boolean, signUpErr: boolean };
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
-// interface DataProviderProps {
-//   children: ReactNode;
-// }
 const UserContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     // vars
     const [ isUserLoading, setIsUserLoading ] = useState<boolean>(false);
@@ -45,6 +46,33 @@ const UserContextProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             setIsEmailAvailable({ isEmailAvailable: false, isEmailAvailableLoading: false, isEmailAvailableErr: true });
         }
     }
+
+    // sign up route to /api/auth/signup - POST
+    // updates the state vars for signUp - { signUpSuccess, signUpLoading, signUpErr }
+    const [ { signUpSuccess, signUpLoading, signUpErr }, setSignUp ] = useState<{ signUpSuccess: boolean, signUpLoading: boolean, signUpErr: boolean }>({ signUpSuccess: false, signUpLoading: false, signUpErr: false });
+    const signUp = async (vars: { email: string, password: string }, source?: any) => {
+        try {
+            const { email, password } = vars;
+            setSignUp({ signUpSuccess: false, signUpLoading: true, signUpErr: false });
+            await axios.post(`http://localhost:3001/api/auth/signup`, { email, password }, { cancelToken: source?.token })
+                .then((response) => {
+                    console.log("Sign up response:", response.data);
+                    setSignUp({ signUpSuccess: true, signUpLoading: false, signUpErr: false });
+
+                    // saves response.data.token to local storage
+                    localStorage.setItem('token', response.data.token);
+                })
+                .catch((error) => {
+                    setSignUp({ signUpSuccess: false, signUpLoading: false, signUpErr: true });
+                    console.error("Error signing up:", error);
+                })
+        }
+        catch (error) {
+            console.error('Error fetching data:', error);
+            setSignUp({ signUpSuccess: false, signUpLoading: false, signUpErr: true });
+        }
+    };
+   
 
     // functions
     const getUser = async (id: string) => {
@@ -76,7 +104,9 @@ const UserContextProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             isUserLoading, 
             getUser,
             isEmailAvailable,
-            getIsEmailAvailable
+            getIsEmailAvailable,
+            signUp,
+            signUpVars: { signUpSuccess, signUpLoading, signUpErr },
         }}>
             {children}
         </UserContext.Provider>
