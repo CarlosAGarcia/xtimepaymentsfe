e999a11259ad7f7ab754a8f72d67559c096d8ee0
Carlos <thethirdcage@gmail.com>
2024-10-18 23:50:13 +0800

    changes

--- src/routes/restrictedPages/Dashboard.tsx
+++ src/routes/restrictedPages/Dashboard.tsx
@@ -1,21 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AuthContext } from '../../contexts/auth/authContext';
-import Header from '../../components/headers/Headers';
-import SubHeader from '../../components/headers/Subheaders';
+import HeaderContentLayout from '../../layouts/HeaderContentLayout'
+import { useOrganisation } from '../../contexts/organisations/organisationContext';
 
 export default function Dashboard() {
+  const { organisation, getOrganisationById, isGetOrganisationByIdLoading, isGetOrganisationByIdErr } = useOrganisation()
   const userContext = useContext(AuthContext);
   if (!userContext) throw new Error('YourComponent must be used within a MainProvider');
 
   const { user, isUserLoading, isUserErr } = userContext;
+  // on load and on every user.organisation._id change, fetch the organisation data with getOrganisationById and the id
+  useEffect(() => {
+      if (!organisation?.name && user?.organisation?._id) {
+          getOrganisationById(user.organisation._id)
+      }
+  }, [ user?.organisation?._id, organisation ])
 
   return (
-    <div>
-      <Header title='DASHBOARD'/>
-      <SubHeader title='' />
+    <HeaderContentLayout title={isGetOrganisationByIdLoading ? 'LOADING...' : organisation?.name} subTitle=''>
       <div>
         {isUserLoading ? 'LOADING...' : isUserErr ? 'ERR' : JSON.stringify(user.email)}
       </div>
-    </div>
+    </HeaderContentLayout>
   )
 }
